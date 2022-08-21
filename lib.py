9809275de2779b638fcf9ae718afa3c92790a1a1
Henri Julius <henri.julius@gmail.com>
2022-08-21 18:21:16 +0200

    Added various

--- lib.py
+++ lib.py
@@ -11,7 +11,23 @@ DRINKS = [
     'H1',
     'H2',
     'H3'
-        ]
+]
+
+UI_TOKEN = {
+        'UI_TOKEN_EXIT': "Beenden ❌",
+        'UI_TOKEN_ROLL': "Würfeln 🎲",
+        'UI_TOKEN_RULES': "Spielanleitung 📜",
+        'UI_TOKEN_SAVE': "Speichern 💾",
+        'UI_TOKEN_LOAD': "Laden 💽"
+}
+
+RULES = """
+REGELN:
+    1. Spieler kennen ihren aktuellen Kontostand nicht.
+    2. Spieler können auf Geld-Feldern zufällig zusätzliches Geld gewinnen (oder verlieren).
+    3. Spieler können auf Grundstück-Feldern diese kaufen, auch wenn sie nicht genug Geld haben, und die anderen Spieler lönnen anzweifeln, ob sie tatsächlich genug Geld für den Kauf haben.
+    4. Spieler konnen auf Duell-Feldern andere Spieler herausfordern (abwechselnd trinken) und um ein Grundstück spielen.
+"""
 
 def yes_no(query,ui,yes_first=False):
     return ui_command([query,"Nein","Ja"],ui) == 2
